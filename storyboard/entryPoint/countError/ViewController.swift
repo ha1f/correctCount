2af24aa27d3a6d2143cfc1411dc41012a2e14f8d
ha1fha1f <half.ldk.hiz@gmail.com>
2016-05-24 23:51:47 +0900

    いろいろ

---
+++ storyboard/entryPoint/countError/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  countError
+//
+//  Created by 山口智生 on 2016/05/24.
+//  Copyright © 2016年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
+
+}
+
